e1f2f67285b5a7275d5c53750e57d3cd1a45b744
yangjiun00 <jiunine4@gmail.com>
2025-07-23 14:51:14 +0900

    black isort mypy 실행

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,11 +1,11 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from apps.cart.views import CartViewSet
 
 router = DefaultRouter()
-router.register(r'', CartViewSet, basename='cart')
+router.register(r"", CartViewSet, basename="cart")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
