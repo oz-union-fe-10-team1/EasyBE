e122874f86c8888a8e740c6ba2e2e9fcf8ec10b5
choismjames23 <choism23@naver.com>
2025-08-05 11:18:59 +0900

    ✨ feat : 모니터링 툴 장착(sentry)
    모니터링 툴 연결
    
    # Conflicts:
    #	config/settings/prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,23 @@
 from typing import Any, Dict
 
 from config.settings.base import *
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Add data like request headers and IP for users;
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=os.getenv("SENTRY_TRACES_SAMPLE_RATE"),
+    # To collect profiles for all profile sessions,
+    # set `profile_session_sample_rate` to 1.0.
+    profile_session_sample_rate=os.getenv("SENTRY_PROFILES_SAMPLE_RATE"),
+    # Profiles will be automatically collected while
+    # there is an active span.
+    profile_lifecycle="trace",
+)
 
 DEBUG = False
 ALLOWED_HOSTS = [host.strip() for host in os.environ.get("ALLOWED_HOSTS", "").split(",") if host.strip()]
