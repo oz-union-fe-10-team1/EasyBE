687db2a494f6608f6d60bd5238d173153f6a81a6
“leejiyun1” <poi20701556@gmail.com>
2025-08-17 13:04:40 +0900

    ♻️ refactor: 통합 서비스 클래스 및 하위 호환성 인터페이스 구현
    - services/base.py: 기존 TasteTestService 인터페이스를 유지하는 통합 서비스 클래스 구현
    - services/__init__.py: 모든 서비스와 하위 호환성 함수들 exports 정의

--- apps/taste_test/services/__init__.py
+++ apps/taste_test/services/__init__.py
@@ -2,12 +2,19 @@
 서비스 모듈
 """
 
+from .base import TasteTestService, get_questions, process_taste_test
 from .calculator import ScoreCalculator
 from .analyzer import TypeAnalyzer
 from .storage import TestResultStorage
 from .profile_handler import ProfileHandler
 
 __all__ = [
+    # 통합 서비스 (하위 호환성)
+    "TasteTestService",
+    "get_questions",
+    "process_taste_test",
+
+    # 개별 서비스 클래스
     "ScoreCalculator",
     "TypeAnalyzer",
     "TestResultStorage",
