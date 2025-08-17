095282c77e3f62277f84d52cb5ed8c59122459f5
“leejiyun1” <poi20701556@gmail.com>
2025-08-17 12:56:34 +0900

    ♻️ refactor: 맛 프로필 데이터를 constants 모듈로 분리
    - constants/taste_profiles.py: 9가지 취향 유형별 기본 맛 프로필 데이터 분리
    - constants/__init__.py: TASTE_PROFILES exports 추가

---
+++ apps/taste_test/constants/__init__.py
@@ -0,0 +1,15 @@
+"""
+취향 테스트 상수 모듈
+"""
+
+from .test_data import QUESTIONS, ANSWER_MAPPING
+from .taste_types import TYPE_INFO, MIXED_TYPE_MAPPING
+from .taste_profiles import TASTE_PROFILES
+
+__all__ = [
+    "QUESTIONS",
+    "ANSWER_MAPPING",
+    "TYPE_INFO",
+    "MIXED_TYPE_MAPPING",
+    "TASTE_PROFILES",
+]
