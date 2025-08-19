200f068c016c9856281f23755f987feae16c49ca
yangjiun00 <jiunine4@gmail.com>
2025-08-19 14:24:22 +0900

    url수정

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -6,12 +6,9 @@ from apps.orders.views import OrderItemListViewSet, OrderViewSet
 app_name = "orders"
 
 router = DefaultRouter()
+router.register(r"orders", OrderViewSet, basename="order")
 router.register(r"order-items", OrderItemListViewSet, basename="order-item")
 
 urlpatterns = [
     path("", include(router.urls)),
-    # OrderViewSet의 URL들을 수동으로 등록
-    path("create_from_cart/", OrderViewSet.as_view({"post": "create_from_cart"}), name="order-create-from-cart"),
-    path("", OrderViewSet.as_view({"get": "list"}), name="order-list"),
-    path("<int:pk>/", OrderViewSet.as_view({"get": "retrieve"}), name="order-detail"),
 ]
